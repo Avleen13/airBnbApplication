d581e569e93aa445b055e826f0bc58e918f74616
Avleen13 <avleen39@gmail.com>
2020-04-09 01:03:36 -0400

    added room models and views

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router();
+const adminRoomModel = require("../models/adminRoom");
 
 router.use(express.static('public'));
 
@@ -17,7 +18,31 @@ router.get("/room-listing",(req,res)=>{
       room : roomModel.getallRooms()
 
   });
-
-
 });
+
+//Route to direct use to Add Task form
+router.get("/create-room",(req,res)=>
+{
+    res.render("room/createRoom");
+});
+
+//Route to process user's request and data when the user submits the add task form
+router.post("/create-room",(req,res)=>
+{
+        const newRoom = {
+            title : req.body.title,
+            price :req.body.price,
+            description : req.body.description,
+            location: req.body.location,
+            featuredroom:req.body.featuredroom
+        }
+
+     const room =  new adminModel(newRoom);
+     task.save()
+     .then(()=>{
+         res.redirect("/room/dashboard")
+     })
+     .catch(err=>console.log(`Error happened when inserting in the database :${err}`));
+});
+
 module.exports=router;
---
+++ models/adminRoom.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//This indicates the shape of the documents that will be entering the database
+  const roomSchema = new Schema({
+   
+    title:
+    {
+      type:String,
+      required:true
+    },
+
+    price:
+    {
+        type:String,
+        required:true
+    },
+
+    description: 
+    {
+        type:String,
+        required:true
+    },
+
+    location:{
+        type: String,
+        required:true
+    },
+    featuredroom:{
+        type: String,
+        required:true
+    },
+    pic:{
+        type:String,
+        required:true
+    },
+   
+  });
+
+  /*
+    For every Schema you create(Create a schema per collection), you must also create a model object. 
+    The model will allow you to perform CRUD operations on a given collection!!! 
+  */
+
+ const adminRoomModel = mongoose.model('Admin rooms', roomSchema);
+
+ module.exports = adminRoomModel;
